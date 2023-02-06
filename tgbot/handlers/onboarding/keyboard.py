461c35b06c42c2328c298a1120018def164910e9
Alexey Potapov <2furius@gmail.com>
2023-02-06 23:11:43 +0300

    Патчи

--- tgbot/handlers/onboarding/keyboard.py
+++ tgbot/handlers/onboarding/keyboard.py
@@ -9,6 +9,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 from attendance.models import Course
 
+
 def make_keyboard_for_start_command() -> InlineKeyboardMarkup:
     buttons = [[
         InlineKeyboardButton(course.name, callback_data=course.pk) for course in Course.objects.all()
